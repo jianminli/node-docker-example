91c1c082c3bbc1e97d7b39a26d573fe668b3bb4d
jianmin li <jian.min.li@hotmail.com>
2016-05-07 17:26:44 +0800

    change

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
-// server.js
 var http = require('http')
-
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  })
-  res.end('Hello World!')
-})
-
-server.listen(8080, function() {
+var url = require('url')
+var server = http.createServer(router({
+  '/': function(req, res) {
+    res.writeHead(200)
+    res.end('Hello world!')
+  },
+  '/bye': function(req, res) {
+    res.writeHead(200)
+    res.end('Bye~')
+  }
+}))
+server.listen(parseInt(process.env.PORT) || 80, function() {
   console.log('Docker DEMO with Node.js is running.')
 })
+module.exports = server
+// A simple router
+function router(routes) {
+  var paths = Object.keys(routes)
+  var regexs = paths.map(function(path) {
+    return new RegExp('^' + path + '$')
+  })
+  return function(req, res) {
+    var reqUrl = url.parse(req.url).pathname
+    for (var i = 0; i < regexs.length; i++) {
+      if (reqUrl.match(regexs[i]) != null) {
+        var path = paths[i]
+        return routes[path].call(server, req, res)
+      }
+    }
+    // Page not found
+    res.writeHead(404, {
+      'Content-Type': 'text/plain'
+    })
+    res.end('Page not found.')
+  }
+}
